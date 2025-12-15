9397c7f4ad3fdaf5e5410f516ee39e6c5ccc790c
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-12-16 03:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setClock() {
+  const now = new Date();
+
+  // seconds
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  // minutes
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  // hours
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+// run every second
+setInterval(setClock, 1000);
+
+// run once immediately (no 1 sec delay)
+setClock();
